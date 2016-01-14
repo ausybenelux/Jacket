bb712865e865e9d7b01eaadd9c02588c065fd8ad
Sarah Willem <sarah.willem@one-agency.be>
2016-01-14 18:14:55 +0100

    Issue-17: updating gulp task for generating favicons + added testfile favicon.png

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,10 @@ var sourcemaps = require("gulp-sourcemaps");
 var scsslint = require('gulp-scss-lint');
 var sassdoc = require("sassdoc");
 
+// Favicons
+var realFavicon = require("gulp-real-favicon");
+var fs = require('fs');
+
 // Jade
 var jade = require('gulp-jade');
 
@@ -46,12 +50,38 @@ var errorCallBack = function (error, metadata) {
 };
 
 // -----------------------------------------------------------------------------
-// FAVICONS -- https://github.com/haydenbleasel/favicons
+// FAVICONS -- http://realfavicongenerator.net/
 // -----------------------------------------------------------------------------
 
+// Generate the icons. This task takes a few seconds to complete. 
+// You should run it at least once to create the icons. Then, 
+// you should run it whenever RealFaviconGenerator updates its 
+// package (see the check-for-favicon-update task below).
 gulp.task("favicons", "Generates cross-device favicons from dist/img/logo/favicon.png", function() {
-  return favicons(config.favicons, errorCallBack);
+  realFavicon.generateFavicon(config.favicons);
 });
+
+// Inject the favicon markups in your HTML pages. You should run 
+// this task whenever you modify a page. You can keep this task 
+// as is or refactor your existing HTML pipeline.
+// gulp.task('inject-favicon-markups', function() {
+//   gulp.src([ 'TODO: List of the HTML files where to inject favicon markups' ])
+//     .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+//     .pipe(gulp.dest('TODO: Path to the directory where to store the HTML files'));
+// });
+
+// Check for updates on RealFaviconGenerator (think: Apple has just
+// released a new Touch icon along with the latest version of iOS).
+// Run this task from time to time. Ideally, make it part of your 
+// continuous integration system.
+// gulp.task('check-for-favicon-update', function(done) {
+//   var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+//   realFavicon.checkForUpdates(currentVersion, function(err) {
+//     if (err) {
+//       throw err;
+//     }
+//   });
+// });
 
 // -----------------------------------------------------------------------------
 // SASS -- https://www.npmjs.com/package/gulp-sass
